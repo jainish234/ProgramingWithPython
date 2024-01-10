e8ef3884dd4d263c97645fa4b564c722df594444
jainish234 <143157651+jainish234@users.noreply.github.com>
2024-01-10 22:25:00 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,26 @@
+a='welcome python program'
+print(a)
+print('welcome')
+print(7)
+
+#comments in python
+a='Atmiya University'
+print(a)
+b='Rajkot'
+print(b)
+
+
+#concatenate in python
+city='Surat'
+print('City is:'+city)
+print('City is',city)
+
+#formatting output using format string 
+print('City is:%s'%city)
+print('City is:(%20s)'%city)
+print('City is:(%-20s)'%city)
+
+#taking input from the user
+age=int(intput('Enter the age:'))
+print(age)
+print(type(age))
